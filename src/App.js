812895ff4e25a2398211b7fd4fd2b96b48e5fb58
varito <varit.as@ku.th>
2019-07-17 17:22:43 +0900

    finish code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// App.js
+import React, { Component } from 'react';
+import Header from './components/Header';
+import MessageList from './components/MessageList';
+import MessageBox from './components/MessageBox';
+import firebase from 'firebase';
+class App extends Component {
+  constructor(props){
+  super(props);
+  var firebaseConfig = {
+    apiKey: "AIzaSyAafvChcmtB_NiNvzK5mJp6uVpuTVI9Mjk",
+    authDomain: "fir-realtimeweb-69681.firebaseapp.com",
+    databaseURL: "https://fir-realtimeweb-69681.firebaseio.com",
+    projectId: "fir-realtimeweb-69681",
+    storageBucket: "",
+    messagingSenderId: "651691851291",
+    appId: "1:651691851291:web:50b4e0bf554e0888"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Simple Firebase App" />
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageList db={firebase} />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageBox db={firebase} />
+        </div>
+      </div>
     </div>
   );
+ }
 }
-
 export default App;
